7d05c39903effba8712ccaf507950c22e3481991
ethiobek <arkishasoftware@gmail.com>
2022-04-20 13:20:59 +0300

    Container and Icon button

--- src/components/SuccessButton.js
+++ src/components/SuccessButton.js
@@ -2,6 +2,9 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import React from "react";
 import { Container } from "@mui/material";
+import DirectionsRun from "@mui/icons-material/DirectionsRun";
+import AlarmOn from "@mui/icons-material/AlarmOn";
+import Accessibility from "@mui/icons-material/Accessibility";
 
 function SuccessButton() {
   return (
@@ -16,9 +19,18 @@ function SuccessButton() {
       </Button>
 
       <ButtonGroup variant="contained">
-        <Button>Set</Button>
-        <Button>Ready</Button>
-        <Button>Go</Button>
+        <Button color="primary">
+          <AlarmOn />
+          Set
+        </Button>
+        <Button color="error">
+          <Accessibility />
+          Ready
+        </Button>
+        <Button color="success">
+          <DirectionsRun />
+          Go
+        </Button>
       </ButtonGroup>
     </Container>
   );
